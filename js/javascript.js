1b5751414cfdd5f3745c57ca12ac95cb1ae3afc2
Shivam Gupta <shivamg11000@gmail.com>
2017-06-07 18:35:17 +0530

    simple image slider v-2.0

--- js/javascript.js
+++ js/javascript.js
@@ -1,10 +1,37 @@
-$(document).ready(function() {
-  $(".img-slider .after-btn").click(function() {
-    $(".img-slider .current").removeClass("current").next().addClass("current");
-  });
 
-  $(".img-slider .prev-btn").click(function() {
-    $(".img-slider .current").removeClass("current").prev().addClass("current");
-  });
+$(document).ready(function() {
+
+  var count = $('.disp-img > img').length;    /* count the numner of images */
+  var i = 1;
+
+
+    /* click the right btn */
+    $(".after-btn").click(function() {
+
+       if (i < count){
+         $(".current").removeClass("current").next().addClass("current");
+         i++;
+       }
+       else {
+         $(".current").removeClass("current");
+         $(".disp-img img:nth-child(1)").addClass("current");
+         i=1;
+       }
+    });
+
+   /* click the left btn */
+    $(".prev-btn").click(function() {
+
+       if (i > 1){
+         $(".current").removeClass("current").prev().addClass("current");
+         i--;
+       }
+       else {
+         $(".current").removeClass("current");
+         $(".disp-img img:nth-child(4)").addClass("current");
+         i=4;
+       }
+    });
+
 
 });
